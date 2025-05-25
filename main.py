f2baedbd9d433546e2e8bd288efba0576759058b
FabreezeGuzzo <81541925+FabreezeGuzzo@users.noreply.github.com>
2025-05-25 18:23:30 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Purpose of this program is to figure out how many decades the users been alive")
+variable = float(input("Your Age"))
+decades = variable / 10
+print("You  have lived", decades, "decades")
+#use the math function sqrt to find the square root of their age
+import math
+root = (math.sqrt(variable))
+print("Square root of your age", root)
+print("Thank you for using my program, adios.")
