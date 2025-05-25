fbf238fdd7a0b8a0742ead563b1bf8b48686c2e7
FabreezeGuzzo <81541925+FabreezeGuzzo@users.noreply.github.com>
2025-05-25 18:22:53 -0400

    Create syntaxerrors.py

---
+++ syntaxerrors.py
@@ -0,0 +1,6 @@
+variable = input("age")
+decades = variabel / 10
+print "You  have lived" decades "decades"
+#use the math function sqrt to find the square root of their age
+root = sqrt(variable)
+print(root)
